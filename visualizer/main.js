16035d1f1e870d1ce73e28f0361b501dd6654795
Denis Czeskleba <D.Czeskleba@googlemail.com>
2025-09-19 21:58:09 +0200

    Update main.js

--- visualizer/main.js
+++ visualizer/main.js
@@ -202,7 +202,16 @@ function update(p){
     const chosen = new Set();
     const h = new Float32Array(hN*3);
     for(let i=0;i<hN;i++){
-      let idx; do { idx = Math.floor(rand()*(allSites.length/3)); }     demo.setH(h, feR * p.hSize);
+      let idx;
+      do {
+        idx = Math.floor(rand() * (allSites.length / 3));
+      } while (chosen.has(idx));
+      chosen.add(idx);
+      h[3*i]   = allSites[3*idx];
+      h[3*i+1] = allSites[3*idx+1];
+      h[3*i+2] = allSites[3*idx+2];
+      }
+      demo.setH(h, feR * p.hSize);
 while (chosen.has(idx));
       chosen.add(idx);
       h[3*i]   = allSites[3*idx];
